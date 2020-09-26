35401234df26038cae809db099bfab6b03d6c2a0
ehower <ethanrhower@gmail.com>
2020-09-26 12:51:32 -0400

    Added ModifyTime method to Event class

--- ScheduleInator/Event.cs
+++ ScheduleInator/Event.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CustomTime;
 
 namespace ScheduleInator
 {
@@ -14,13 +15,18 @@ namespace ScheduleInator
         private string type { get; set; }
         private int dueDate { get; set; }
 
-        public Event(int startTime, int endTime, string name, string type, int dueDate, CustomTime customTime)
+        public Event(int startTime, int endTime, string name, string type, int dueDate, CustomTime specifiedTime,CustomTime dueDate )
         {
             this.startTime = startTime;
             this.endTime = endTime;
             this.name = name;
             this.type = type;
             this.dueDate = dueDate;
+        }
+
+        public ModifyTime(int day, Time time)
+        {
+            this.specifiedTime.addTimeToDay(day, time)
         }
     }
 }
