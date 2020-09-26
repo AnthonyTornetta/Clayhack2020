4cf534c3df46440567e4446cec652ddba6792851
Cornchip <cornchipgonecodin@gmail.com>
2020-09-26 12:30:07 -0400

    Update CustomTime.cs

--- ScheduleInator/CustomTime.cs
+++ ScheduleInator/CustomTime.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace ScheduleInator
 {
+    /// <summary>
+    /// Represents a collection of times that can occur during a week
+    /// </summary>
     class CustomTime
     {
+        List<Time>[] times;
 
+        public CustomTime()
+        {
+            times = new List<Time>[7];
+        }
+
+        /// <summary>
+        /// Gets the times for a given day
+        /// </summary>
+        /// <param name="day">0-7 = sunday-saturday</param>
+        /// <returns></returns>
+        public List<Time> getTimesForDay(int day)
+        {
+            return times[day];
+        }
+
+        public void addTimeToDay(int day, Time time)
+        {
+            times[day].Add(time);
+        }
     }
 }
