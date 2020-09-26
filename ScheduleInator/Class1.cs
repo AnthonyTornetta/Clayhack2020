6efbe747a75b36f3cac9d967e1551892bc5959cf
Jcdiazpeon <44230501+Jcdiazpeon@users.noreply.github.com>
2020-09-26 12:30:24 -0400

    Create Class1.cs

---
+++ ScheduleInator/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleInator
+{
+    class Event
+    {
+        private int startTime { get; set; }
+        private int endTime { get; set; }
+        private string name { get; set; }
+        private string type { get; set; }
+        private int dueDate { get; set; }
+
+        public Event(int startTime, int endTime, string name, string type, int dueDate, CustomTime customTime)
+        {
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.name = name;
+            this.type = type;
+            this.dueDate = dueDate;
+        }
+    }
+}
